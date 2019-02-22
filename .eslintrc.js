90cd56a88264a4437068b1a49a5f42091dccb930
naoto67 <uver.com67@gmail.com>
2019-02-22 22:47:10 +0900

    initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+  ],
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {}
+}
